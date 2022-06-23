c732e9c5fd32210b035750bd59055b433156d293
gavuolo <gabs.vuolo@gmail.com>
2022-06-23 16:09:00 -0400

    Desenv. Layout

---
+++ script.js
@@ -0,0 +1,17 @@
+//linkar o axios
+//fazer a mensagem ser enviada para o axios
+// capturar os textos enviados do axios e transferir para as divs do chat
+// input.inerHTML para pegasr o que foi digitado no input
+
+function buscarMensagem(){
+
+
+    
+
+
+}
+
+function enviarMensagem(){
+
+
+}
