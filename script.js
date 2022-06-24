078ebc28e46809fd00b34a1a6751314ed9481750
gavuolo <gabs.vuolo@gmail.com>
2022-06-24 18:35:32 -0400

    Buscar msgs API

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 //linkar o axios
 //fazer a mensagem ser enviada para o axios
 // capturar os textos enviados do axios e transferir para as divs do chat
-// input.inerHTML para pegasr o que foi digitado no input
+// input.inerHTML.value para pegasr o que foi digitado no input
 
+//mensagem: https://mock-api.driven.com.br/api/v6/uol/messages
+
+//participante: https://mock-api.driven.com.br/api/v6/uol/participants 
+
+let menssagemEnviada = []
+
+//user.push(mensaggem)
+
+let usuario = [{
+    name: "",
+}]
+
+buscarMensagem()
 function buscarMensagem(){
 
+    let promessa =  axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+        
 
-    
+   promessa.then(alerta => {
+    console.log(alerta)
+    console.log(alerta.data)
+    menssagem = alerta.data
+    renderizarMensagem()
+    })
+
+    promessa.catch(error => {
+        console.log(error)
+    })
 
 
 }
 
-function enviarMensagem(){
+function renderizarMensagem(){
+
+    let chat = document.querySelector('.chat')
+      
+        for(let i = 0; i < menssagem.length; i++){
+            
+            chat.innerHTML += `<div class="user"> (${menssagem[i].time}) ${menssagem[i].from} para ${menssagem[i].to}: ${menssagem[i].text} </div>`
+
+        }
 
 
 }
+
+//{from: 'aa', to: 'Todos', text: 'entra na sala...', type: 'status', time: '05:35:02'}
